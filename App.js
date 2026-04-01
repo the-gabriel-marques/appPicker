363298d49314cbe8c3cebf215f4a9e5c27051c14
the-gabriel-marques <gabrielmarquesdasilvacontato@gmail.com>
2026-04-01 15:08:56 -0300

    inicio

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const [animal, setAnimal] = useState('');
+
+return (
+<View style={styles.app}>
+<Text>Escolha um animal:</Text>
+
+<Picker selectedValue={animal}
+onValueChange={(itemValue) => setAnimal(itemValue)}>
+
+<Picker.Item label="Selecione..." value="" />
+<Picker.Item label="Cachorro" value="Cachorro" />
+<Picker.Item label="Gato" value="Gato" />
+<Picker.Item label="Pássaro" value="Pássaro" />
+</Picker>
+
+<Text > Você escolheu: {animal} </Text>
+</View>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+app: {
+flex: 1,
+justifyContent: 'center',
+padding: 20,
+},
 });
